7c0d68899468c898e762ac119bad253ab21dcc19
Daniel Malyszczuk <malyszczuk.daniel@gmail.com>
2017-03-16 22:21:44 +0100

    GC Performance

--- src/test/java/ConvertorJSONTest.java
+++ src/test/java/ConvertorJSONTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class ConvertorJSONTest {
 
     @Test
-    public void parseFromStudentClass() throws Exception{
+    public void convertFromStudentClass() throws Exception{
         //given
         Student sut = new Student(1, "Daniel", "Malyszczuk");
         String expected = new String("{\"surname\":Malyszczuk,\"id\":\"1\",\"name\":\"Daniel\"}");
