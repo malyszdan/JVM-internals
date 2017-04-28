17611dcd56078a95c098739cb780216c23270a15
Daniel Malyszczuk <Daniel.Malyszczuk@smart4aviation.aero>
2017-04-28 11:52:40 +0200

    streams
    Change-Id: I375250dd158551b18dc4c1ffc22de89e1ef46ebe

--- streams/src/main/java/services/CustomerService.java
+++ streams/src/main/java/services/CustomerService.java
@@ -1,17 +1,16 @@
 package services;
 
 import java.lang.reflect.Field;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Customer;
 import entities.Product;
+
 import org.apache.commons.lang3.reflect.FieldUtils;
-import org.apache.commons.math3.util.Precision;
 
 public class CustomerService implements CustomerServiceInterface {
 
@@ -23,49 +22,44 @@ public class CustomerService implements CustomerServiceInterface {
 
     @Override
     public List<Customer> findByName(String name) {
-        return customers
-                .stream()
-                .filter(customer -> name.equals(customer.getName()))
-                .collect(Collectors.toList());
+        return customers.stream().filter(customer -> name.equals(customer.getName())).collect(Collectors.toList());
     }
 
     @Override
     public List<Customer> findByField(String fieldName, Object value) {
-        return customers
-                .stream()
-                .filter(customer -> {
-                    Field field = FieldUtils.getField(customer.getClass(), fieldName, true);
-                    try {
-                        return value.equals(field.get(customer));
-                    } catch (IllegalAccessException e) {
-                        return false;
-                    }
-                }).collect(Collectors.toList());
+        return customers.stream().filter(customer -> {
+            Field field = FieldUtils.getField(customer.getClass(), fieldName, true);
+            try {
+                return value.equals(field.get(customer));
+            } catch (IllegalAccessException e) {
+                return false;
+            }
+        }).collect(Collectors.toList());
     }
-
 
     @Override
     public List<Customer> customersWhoBoughtMoreThan(int number) {
-        return customers.stream()
-                .filter(customer -> customer.getBoughtProducts().size() > number)
-                .collect(Collectors.toList());
+        return customers.stream().filter(customer -> customer.getBoughtProducts().size() > number).collect(Collectors.toList());
     }
 
     @Override
     public List<Customer> customersWhoSpentMoreThan(double price) {
-
-    }
-
-    @Override
-    public List<Customer> customersWithNoOrders() {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public void addProductToAllCustomers(Product p) {
-        // TODO Auto-generated method stub
+    public List<Customer> customersWithNoOrders() {
+        return customers.stream().filter(customer -> customer.getBoughtProducts().isEmpty()).collect(Collectors.toList());
+    }
 
+    @Override
+    public void addProductToAllCustomers(Product p) {
+        if ( p != null ) {
+            customers.stream().forEach(customer -> {
+                customer.addProduct(p);
+            });
+        }
     }
 
     @Override
@@ -76,14 +70,20 @@ public class CustomerService implements CustomerServiceInterface {
 
     @Override
     public boolean wasProductBought(Product p) {
-        // TODO Auto-generated method stub
-        return false;
+        return customers.stream().anyMatch(customer -> customer.getBoughtProducts().contains(p));
     }
 
     @Override
     public List<Product> mostPopularProduct() {
-        // TODO Auto-generated method stub
-        return null;
+       return customers.stream()
+                // map person to tag & filter null tag out
+                .map(customers -> customers.getBoughtProducts()).filter(Objects::nonNull)
+                // summarize tags
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                // fetch the max entry
+                .entrySet().stream().max(Map.Entry.comparingByValue())
+                // map to tag
+                .map(Map.Entry::getKey).orElse(null);
     }
 
     @Override
--- streams/src/test/java/tests/CustomerServiceTest.java
+++ streams/src/test/java/tests/CustomerServiceTest.java
@@ -1,14 +1,15 @@
 package tests;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import entities.Product;
 import services.CustomerService;
 import services.CustomerServiceInterface;
 import entities.Customer;
@@ -16,16 +17,21 @@ import entities.Customer;
 public class CustomerServiceTest {
 
     private static CustomerServiceInterface cs;
+
+    private static Product product;
+
     private List<Customer> res;
+
     private List<Customer> res2;
 
     @BeforeClass
     public static void setUp() {
         cs = new CustomerService(DataProducer.getTestData(10));
+        product = new Product(11, "Product: 11", Double.valueOf(11));
     }
 
     @Test
-    public void testFindByName() throws Exception{
+    public void testFindByName() throws Exception {
         res = cs.findByName("Customer: 1");
 
         assertNotNull("Result can't be null", res);
@@ -42,22 +48,42 @@ public class CustomerServiceTest {
     }
 
     @Test
-    public void customersWhoBoughtMoreThanTest() throws Exception{
-        res = cs.customersWhoBoughtMoreThan(0);
+    public void customersWhoBoughtMoreThanTest() throws Exception {
         res2 = cs.customersWhoBoughtMoreThan(7);
 
-        assertEquals(7, res.size());
-        assertEquals(Collections.<Customer>emptyList(), res2);
+        assertEquals(7, cs.customersWhoBoughtMoreThan(0).size());
+        assertEquals(Collections.<Customer> emptyList(), res2);
     }
 
     @Test
-    public void customersWhoSpentMoreThanTest() throws Exception{
-        res = cs.customersWhoSpentMoreThan(90);
-        res2 = cs.customersWhoSpentMoreThan(50);
+    public void customersWithNoOrdersTest() throws Exception {
+        res = cs.customersWithNoOrders();
 
-        assertEquals(0, res.size());
-        assertEquals(5, res2.size());
+        assertThat(res.isEmpty(), is(false));
     }
 
+    @Test
+    public void addProductToAllCustomersTest() throws Exception {
+        CustomerService cs2 = new CustomerService(DataProducer.getTestData(10));
 
+        cs2.addProductToAllCustomers(product);
+
+        assertTrue(cs.customersWhoBoughtMoreThan(0).size() < cs2.customersWhoBoughtMoreThan(0).size());
+    }
+
+    @Test
+    public void wasProductBought() throws Exception {
+        CustomerService cs2 = new CustomerService(DataProducer.getTestData(10));
+
+        assertFalse(cs2.wasProductBought(product));
+        cs2.addProductToAllCustomers(product);
+        assertTrue(cs2.wasProductBought(product));
+    }
+
+    @Test
+    public void mostPopularProductTest() throws Exception {
+        cs.addProductToAllCustomers(product);
+
+        assertTrue(cs.mostPopularProduct().get(0).equals(product));
+    }
 }
